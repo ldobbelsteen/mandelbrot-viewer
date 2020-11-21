fc152e1a7a4714ab1b995796796df93bc2464189
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2020-11-21 18:10:23 +0100

    Add minimum and maximum zoom levels to prevent running out of precision

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import './styles.css'
 import FractalLayer from './fractal-layer.js'
 import Leaflet from 'leaflet'
 
-const leaflet = Leaflet.map('leaflet', { crs: Leaflet.CRS.Simple })
+const options = {
+  attributionControl: false,
+  crs: Leaflet.CRS.Simple,
+  minZoom: 1,
+  maxZoom: 45
+}
+
+const leaflet = Leaflet.map('leaflet', options)
 leaflet.setView([0, 0], 1)
 leaflet.addLayer(new FractalLayer())

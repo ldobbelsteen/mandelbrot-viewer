20e45f8c77911335bf7c4e711501f7ff96dedec8
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2020-11-25 11:59:48 +0100

    Move Mandelbrot logic to separate module and micro-optimize it

---
+++ src/mandelbrot.js
@@ -0,0 +1,50 @@
+/**
+ * Takes a point on the complex plane (with a real and an imaginary part) and
+ * determines whether it is a part of the Mandelbrot set. The output is the
+ * ratio of the iterations of the Mandelbrot function it took to reach a
+ * modulus of 2 and the maximum iterations. The output is 1 if the point
+ * doesn't escape before maxIterations number of iterations.
+ */
+export default (real, imaginary, maxIterations) => {
+  if (inCardioid(real, imaginary)) return 1
+  if (inBulb(real, imaginary)) return 1
+  let x = 0
+  let y = 0
+  let xSquared = 0
+  let ySquared = 0
+  let iterations = 0
+  while (xSquared + ySquared <= 4) {
+    y = 2 * x * y + imaginary
+    x = xSquared - ySquared + real
+    xSquared = Math.pow(x, 2)
+    ySquared = Math.pow(y, 2)
+    iterations++
+    if (iterations === maxIterations) return 1
+  }
+  iterations += smoothingOffset(xSquared + ySquared)
+  return iterations / maxIterations
+}
+
+// Calculate offset of iterations to achieve smoother coloring
+function smoothingOffset (dotProduct) {
+  const log = Math.log2(Math.log2(dotProduct))
+  const offset = 4 - log
+  return offset
+}
+
+// Check whether a point on the complex plane is in the main cardioid
+function inCardioid (real, imaginary) {
+  const realCenter = real - 0.25
+  const imaginarySquared = Math.pow(imaginary, 2)
+  const circle = Math.pow(realCenter, 2) + imaginarySquared
+  const cardioid = circle * (circle + realCenter)
+  const inCardioid = cardioid <= 0.25 * imaginarySquared
+  return inCardioid
+}
+
+// Check whether a point on the complex plane is in the second bulb
+function inBulb (real, imaginary) {
+  const bulb = Math.pow(real + 1, 2) + Math.pow(imaginary, 2)
+  const inBulb = bulb <= 0.0625
+  return inBulb
+}
--- src/worker.js
+++ src/worker.js
@@ -1,54 +1,23 @@
-onmessage = (event) => {
-  const {
-    image,
-    maxIterations,
-    pixelSize,
-    realSize,
-    x,
-    y
-  } = event.data
+import Mandelbrot from './mandelbrot.js'
 
+onmessage = (event) => {
+  const {image, pixelSize, x, y, realSize, maxIterations} = event.data
   let index = 0
-  const pixels = image.data
   const scale = realSize / pixelSize
-  
   for (let pY = 0, rY = y; pY < pixelSize; pY++, rY -= scale) {
     for (let pX = 0, rX = x; pX < pixelSize; pX++, rX += scale) {
-      const fraction = mandelbrot(rX, rY, maxIterations)
+      const fraction = Mandelbrot(rX, rY, maxIterations)
       const colors = hueToRgb(fraction)
-      pixels[index++] = colors[0]
-      pixels[index++] = colors[1]
-      pixels[index++] = colors[2]
-      pixels[index++] = 255
+      image.data[index++] = colors[0]
+      image.data[index++] = colors[1]
+      image.data[index++] = colors[2]
+      image.data[index++] = 255
     }
   }
-
   postMessage(image, [image.data.buffer])
 }
 
-function mandelbrot (x0, y0, maxIterations)  {
-  const inCardiod = 8 * Math.pow(x0, 4) + Math.pow(x0, 2) * (16 * Math.pow(y0, 2) - 3) + x0 + 8 * Math.pow(y0, 4) - 3 * Math.pow(y0, 2) <= 0.09375
-  const inBulb = Math.pow(x0, 2) + 2 * x0 + Math.pow(y0, 2) <= -0.9375
-  if (inCardiod || inBulb) return 1
-  let x = 0
-  let y = 0
-  let xSquared = 0
-  let ySquared = 0
-  let iterations = 0
-  while (xSquared + ySquared <= 4 && iterations < maxIterations) {
-    y = 2 * x * y + y0
-    x = xSquared - ySquared + x0
-    xSquared = x * x
-    ySquared = y * y
-    iterations++
-  }
-  if (iterations < maxIterations) {
-    const smoothFactor = Math.log(Math.log(xSquared + ySquared) / (2 * Math.log(2))) / Math.log(2)
-    iterations += 1 - smoothFactor
-  }
-  return iterations / maxIterations
-}
-
+// Convert a fraction with 0 <= fraction <= 1 to an RGB color
 function hueToRgb (fraction) {
   const hue = fraction * 6
   const intensity = 255 * (1 - Math.abs((hue % 2) - 1))
